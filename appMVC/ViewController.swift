bacfae722231516d92129f0d4a8865de4745be00
mayuka.kawamukai <mayuka.kawamukai@glpgs.com>
2018-09-12 16:29:35 +0900

    Initial Commit

---
+++ appMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appMVC
+//
+//  Created by mayuka.kawamukai on 2018/09/12.
+//  Copyright © 2018年 mayuka.kawamukai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
